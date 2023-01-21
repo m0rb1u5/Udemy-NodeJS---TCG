362e38d58b04c21b08ebd5932e2abf4a444d8a75
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2023-01-21 16:10:03 -0300

    Section 3 - 28

---
+++ Section 3/28/app.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+const http = require('http');
+
+const server = http.createServer(
+    // eslint-disable-next-line no-unused-vars
+    (req, res) => {
+        console.log(req.url, req.method, req.headers);
+    },
+);
+
+server.listen(3000);
