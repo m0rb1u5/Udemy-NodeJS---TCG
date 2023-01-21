f84962c986a7562e5228d47e99b82aeca0b4c41c
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2023-01-21 15:06:45 -0300

    Config Airbnb style

--- Section 3/26/app.js
+++ Section 3/26/app.js
@@ -3,8 +3,8 @@ const http = require('http');
 const server = http.createServer(
     // eslint-disable-next-line no-unused-vars
     (req, res) => {
-        console.log(req)
-    }
-)
+        console.log(req);
+    },
+);
 
-server.listen(3000)
+server.listen(3000);
