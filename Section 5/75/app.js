a11c7040176e90f9b9e2dedc35dfe52a9144449a
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2023-02-11 21:36:39 -0300

    Section 5 - 75

---
+++ Section 5/75/app.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// eslint-disable-next-line no-unused-vars
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
---
+++ Section 5/75/routes/admin.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+// eslint-disable-next-line no-unused-vars
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+// eslint-disable-next-line no-unused-vars
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
