68dc0a7118bc20c090701c25febf1a87f72ce19c
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2023-02-19 04:36:24 -0300

    Section 6 - 90

---
+++ Section 6/90/app.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+// eslint-disable-next-line no-unused-vars
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+app.listen(3000);
