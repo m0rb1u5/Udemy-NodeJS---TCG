873e7e13f56acb48007b8576c008df29ec4db0f6
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2023-02-19 04:21:40 -0300

    Section 6 - 89

---
+++ Section 6/89/routes/shop.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+const express = require('express');
+
+const adminData = require('./admin');
+
+const router = express.Router();
+
+// eslint-disable-next-line no-unused-vars
+router.get('/', (req, res, next) => {
+    const { products } = adminData;
+    res.render('shop', {
+        prods: products, pageTitle: 'Shop', path: '/', hasProducts: (products.length > 0),
+    });
+});
+
+module.exports = router;
