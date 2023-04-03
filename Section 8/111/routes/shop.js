36e025e56e5dc4f18b421c707efe0f489684407f
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2023-04-03 20:26:25 -0300

    Section 8 - 111

---
+++ Section 8/111/routes/shop.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
