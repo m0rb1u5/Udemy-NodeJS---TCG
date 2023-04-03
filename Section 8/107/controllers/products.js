31780790612b936bd5c649cb06adf57b2e90747d
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2023-04-03 19:47:00 -0300

    Section 8 - 107

---
+++ Section 8/107/controllers/products.js
@@ -0,0 +1,32 @@
+const Product = require('../models/product');
+
+// eslint-disable-next-line no-unused-vars
+exports.getAddProduct = (req, res, next) => {
+    res.render('admin/add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        activeAddProduct: true,
+        productCSS: true,
+    });
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/');
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: (products.length > 0),
+            activeShop: true,
+            productCSS: true,
+        });
+    });
+};
---
+++ Section 8/107/models/product.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'products.json',
+);
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+};
+
+module.exports = class Product {
+    constructor(t) {
+        this.title = t;
+    }
+
+    save() {
+        getProductsFromFile((products) => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err2) => {
+                console.log(err2);
+            });
+        });
+    }
+
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    }
+};
