501034e7c09927c120e3c7cdf62ccc7db85b5f92
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2023-04-03 20:19:26 -0300

    Section 8 - 110

---
+++ Section 8/110/controllers/shop.js
@@ -0,0 +1,39 @@
+const Product = require('../models/product');
+
+// eslint-disable-next-line no-unused-vars
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'All Products',
+            path: '/products',
+        });
+    });
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/index', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+        });
+    });
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.getCart = (req, res, next) => {
+    res.render('shop/cart', {
+        pageTitle: 'Your Cart',
+        path: '/cart',
+    });
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+        pageTitle: 'Checkout',
+        path: '/checkout',
+    });
+};
