1a1798729ee730df29620460fad1929624ae8d52
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2023-04-04 16:09:53 -0300

    Section 9 - 119

---
+++ Section 9/119/controllers/shop.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-console */
+const Product = require('../models/product');
+
+// eslint-disable-next-line no-unused-vars
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'All Products',
+            path: '/products',
+        });
+    });
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    console.log(prodId);
+    res.redirect('/');
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/index', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+        });
+    });
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.getCart = (req, res, next) => {
+    res.render('shop/cart', {
+        pageTitle: 'Your Cart',
+        path: '/cart',
+    });
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        pageTitle: 'Your Orders',
+        path: '/orders',
+    });
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+        pageTitle: 'Checkout',
+        path: '/checkout',
+    });
+};
